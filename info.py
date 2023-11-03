61c11cbda31ae9e74e58081311b9229dfb44ec1f
Thirank <116568300+Thirank@users.noreply.github.com>
2023-11-03 22:26:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 CACHE_TIME = int(environ.get('CACHE_TIME', 99999))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/5250452a1ab5364cfc4dc.jpg https://telegra.ph/file/ebad2eb29a3d2702e1531.jpg')).split()
+PICS = (environ.get('PICS', 'https://graph.org/file/84811a2faa4fa5ce424e5.mp4')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/5250452a1ab5364cfc4dc.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://te.legra.ph/file/6f55d902f9bf2d0afd4bb.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
