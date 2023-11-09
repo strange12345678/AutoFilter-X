e3e16c2f0bc4c153124a700b56928b2b50cbabb1
Thirank <116568300+Thirank@users.noreply.github.com>
2023-11-09 12:35:08 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -7,14 +7,14 @@ from pyrogram.errors import UserIsBlocked, PeerIdInvalid
 async def accept_request(client, r):
 
     rm = InlineKeyboardMarkup([[
-        InlineKeyboardButton("❤️‍🔥 𝖡𝖫𝖠𝖲𝖳𝖤𝖱 𝖧𝖴𝖡 ❤️‍🔥", url=f"https://t.me/blaster_hub"),
-        InlineKeyboardButton("⚡𝖴𝗉𝖽𝖺𝗍𝖾𝗌 ⚡", url=f"https://t.me/piroxbots")
+        InlineKeyboardButton("❤️‍🔥 Fɪʟᴍ Cᴏᴍᴘᴀɴʏ ❤️‍🔥", url=f"https://t.me/Mc_Group_Films"),
+        InlineKeyboardButton("⚡𝖴𝗉𝖽𝖺𝗍𝖾𝗌 ⚡", url=f"https://t.me/Mc_Films_Update")
     ]])
     
     try:
         await client.send_photo(
             r.from_user.id,
-            'https://graph.org/file/5cb80fa6096997b7226b3.jpg',
+            'https://graph.org/file/f1a174694703f9fa9bc92.jpg',
             f"**𝖧𝖾𝗅𝗅𝗈 {r.from_user.mention} 👻, 𝖶𝖾𝗅𝖼𝗈𝗆𝖾 𝖳𝗈 {r.chat.title}\n𝖸𝗈𝗎𝗋 𝖱𝖾𝗊𝗎𝖾𝗌𝗍 𝖧𝖺𝗌 𝖡𝖾𝖾𝗇 𝖠𝗉𝗉𝗋𝗈𝗏𝖾𝖽...!!!**",
             reply_markup=rm)
 
